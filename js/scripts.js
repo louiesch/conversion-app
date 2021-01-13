49e9a202c974578581c819817bfcc33bcdf80215
Louie Schoenknecht <luisesch97@gmail.com>
2021-01-13 15:46:00 -0800

    add fahrenheit to celsius conversion option

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,25 @@ function galToLit(number1) {
   return result;
 };
 
+function fToC(number1) {
+	let result = (number1 - 32) * .5556;
+	return result;
+};
+
 // Below is UI logic
 
 $(document).ready(function() {
   $("form#galToLit").submit(function(event) {
-    event.preventDefault();
+  event.preventDefault();
     let number1 = parseInt($("#gallons").val());
     let result = galToLit(number1).toFixed(2);
     $("#outputLiters").text(result);
-  });
+	});
+	
+	$("form#fToC").submit(function(event) {
+		event.preventDefault();
+			let number1 = parseInt($("#fahrenheit").val());
+			let result = fToC(number1).toFixed(2);
+			$("#outputCelsius").text(result);
+		});
 });
